de5a6fe325cbe1708ccbfe809f6c0002f971990c
Saravanakumar <pskumarsrec88@gmail.com>
2015-10-02 00:00:12 +0530

    User management modification added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   has_many :team_users
   has_many :teams, through: :team_users
   
+  #Validations
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  
 end
